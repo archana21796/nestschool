172b0e3f63a0386e40514eb8fd406ca6023411ac
archana21796 <archana21796@gmail.com>
2025-05-23 19:12:25 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@
       }
     });
   });
+
+  $('.home-slider').owlCarousel({
+  items: 1,
+  loop: true,
+  nav: true,
+  dots: true,
+  smartSpeed: 1000,
+  mouseDrag: true,
+  touchDrag: window.innerWidth > 768, 
+  responsive: {
+    0: { touchDrag: false },
+    768: { touchDrag: true }
+  }
+});
